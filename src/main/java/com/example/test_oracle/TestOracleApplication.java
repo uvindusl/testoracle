c823b4662308218a95c3e608b070c276487b1ca9
Uvindu Sankalpa <uvindusankalpa2021@gmail.com>
2025-03-27 19:09:08 +0530

    git int

---
+++ src/main/java/com/example/test_oracle/TestOracleApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_oracle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestOracleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestOracleApplication.class, args);
+	}
+
+}
