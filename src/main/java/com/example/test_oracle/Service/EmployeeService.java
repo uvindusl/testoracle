b7020ffc91bb9228f67d8931172c9fa464127751
Uvindu Sankalpa <uvindusankalpa2021@gmail.com>
2025-03-27 19:38:42 +0530

    Add Oracle datasource configuration, add new dependencies and EmployeeService class

---
+++ src/main/java/com/example/test_oracle/Service/EmployeeService.java
@@ -0,0 +1,14 @@
+package com.example.test_oracle.Service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+}
